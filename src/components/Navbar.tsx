f3fb9d801ddc2dc5e74a7d394052f41c924c7ded
salimv <salimvzqz@gmail.com>
2025-05-28 14:24:43 -0600

    :hammer: Projects option hidden

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -38,7 +38,7 @@ const Navbar: React.FC<NavbarProps> = ({ darkMode, toggleDarkMode }) => {
     { name: t('nav.about'), href: '#about' },
     { name: t('nav.skills'), href: '#skills' },
     { name: t('nav.experience'), href: '#experience' },
-    { name: t('nav.projects'), href: '#projects' },
+    // { name: t('nav.projects'), href: '#projects' },
     { name: t('nav.certifications'), href: '#certifications' },
     { name: t('nav.contact'), href: '#contact' },
   ];
